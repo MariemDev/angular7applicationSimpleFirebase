0b9607a826501ced718f3f3142cc328c5433a956
BADIS Mariem <badis.mariem.pro@gmail.com>
2019-05-27 14:57:59 +0200

    first commit simple applicaiton angular with deployment firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAfe_y7rFHlofk78r-pXlc6hydw3It5X9I",
+    authDomain: "bird-galaxy-52391.firebaseapp.com",
+    databaseURL: "https://bird-galaxy-52391.firebaseio.com",
+    projectId: "bird-galaxy-52391",
+    storageBucket: "bird-galaxy-52391.appspot.com",
+    messagingSenderId: "105651064003",
+    appId: "1:105651064003:web:d983dace384f6a01"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAfe_y7rFHlofk78r-pXlc6hydw3It5X9I",
+    authDomain: "bird-galaxy-52391.firebaseapp.com",
+    databaseURL: "https://bird-galaxy-52391.firebaseio.com",
+    projectId: "bird-galaxy-52391",
+    storageBucket: "bird-galaxy-52391.appspot.com",
+    messagingSenderId: "105651064003",
+    appId: "1:105651064003:web:d983dace384f6a01"
+  }
 };
 
 /*
